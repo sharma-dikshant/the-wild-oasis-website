4e6ea7d8221243b16b20f14c9fce6c045f175114
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-10 08:56:46 +0530

    setup navigation between pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>welcome to wild oasis</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>welcome to wild oasis</h1>
+    </div>
+  );
 }

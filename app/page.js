2802fc28230997dd575eabc059e4e1821f0dbb7b
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-10 09:20:13 +0530

    created an app layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>welcome to wild oasis</h1>
     </div>
   );

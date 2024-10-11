26c9f09b6cfd8c8d2e48e11d459cb2c4cd9fda2d
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-11 17:12:55 +0530

    created first client component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,18 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
   // console.log(data);
   return (
     <div>
-      <h1>cabins page</h1>;
+      <h1>cabins page</h1>
       <ul>
         {data?.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter/>
     </div>
   );
 }

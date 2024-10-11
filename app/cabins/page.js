077552e4816c11a3ef3449dc8a4e0e8207b2cfcf
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-11 18:08:15 +0530

    renamed components folder to _components to make it secret or to avoid routing to components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,4 @@
-import Counter from "../components/Counter";
+import Counter from "../_components/Counter";
 
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -12,7 +12,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
-      <Counter users={data}/>
+      <Counter users={data} />
     </div>
   );
 }

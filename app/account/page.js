ff470e02451292ce59c5bf40ecfb1eaae4c8d300
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-12 17:57:14 +0530

    added nested routes and pages for account page

--- app/account/page.js
+++ app/account/page.js
@@ -2,7 +2,11 @@ export const metadata = {
   title: "Guest Area",
 };
 function page() {
-  return <div>your account</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Dikshant
+    </h2>
+  );
 }
 
 export default page;

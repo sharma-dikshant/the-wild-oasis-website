bcc31d5074ad3c3d4bc235e0604933152d6efce1
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-26 16:07:10 +0530

    using context api for state management of the reservations

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,7 +1,9 @@
 "use client";
 import { isWithinInterval } from "date-fns";
+import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -14,12 +16,12 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ cabin, settings, bookedDates }) {
+  const { range, setRange, resetRange } = useReservation();
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -31,11 +33,14 @@ function DateSelector({ cabin, settings, bookedDates }) {
         mode="range"
         min={minBookingLength + 1}
         max={maxBookingLength}
-        fromMonth={new Date()}
-        fromDate={new Date()}
-        toYear={new Date().getFullYear() + 5}
+        onSelect={setRange}
+        selected={range}
+        startMonth={new Date()}
+        startDate={new Date()}
+        hidden={isAlreadyBooked(range, bookedDates)}
+        toYear={new Date().getFullYear() + 1}
         captionLayout="dropdown"
-        numberOfMonths={2}
+        numberOfMonths={1}
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
@@ -69,7 +74,7 @@ function DateSelector({ cabin, settings, bookedDates }) {
         {range.from || range.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>

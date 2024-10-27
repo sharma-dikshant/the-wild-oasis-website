4f09ec09191943a3567aa19c0381f2dbcade3003
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-27 16:14:56 +0530

    updated metadata for login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,9 @@
 import SignInButton from "../_components/SignInButton";
 
+export const metadata = {
+  title: "Login"
+}
+
 export default function Page() {
   return (
     <div className="flex flex-col gap-10 mt-10 items-center">

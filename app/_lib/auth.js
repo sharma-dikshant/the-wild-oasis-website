06d767a7d7de72df014675091c429ff59c9dac3f
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-27 16:08:13 +0530

    building a custom signIn page

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -13,9 +13,14 @@ const authCofig = {
       return !!auth?.user;
     },
   },
+  pages: {
+    signIn: "/login",
+  },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authCofig);

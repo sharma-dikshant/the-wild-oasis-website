0ddd87987d2102946019b6d06c09364d00ac41f1
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-27 16:12:14 +0530

    updated name in account section

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
-function page() {
+async function page() {
+  const session = await auth();
+  const firstname = session.user.name.split(" ").at(0);
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Dikshant
+      Welcome, {firstname}
     </h2>
   );
 }

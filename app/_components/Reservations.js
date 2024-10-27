3ec81951ef641fafdcde824292895409ed35aa3c
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-10-27 15:12:45 +0530

    conditionally render some part of app if there is a user session

--- app/_components/Reservations.js
+++ app/_components/Reservations.js
@@ -1,5 +1,7 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservations({ cabin }) {
@@ -7,6 +9,7 @@ async function Reservations({ cabin }) {
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector
@@ -14,7 +17,11 @@ async function Reservations({ cabin }) {
         settings={settings}
         bookedDates={bookedDates}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
